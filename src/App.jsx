c04515242c6bde0780882d063595cc73cbfe3974
Tyler Hodges <33301516+HedgeHodge@users.noreply.github.com>
2021-10-12 20:05:19 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+import SwitchInput from './SwitchInput';
 
 function App() {
+  const [typing, setTyping] = useState(false);
+
   return (
-        <main>
-          <div className="flex container">
-            <div>
-              <ul id="to-dos"></ul>
-            </div>
-            <div className="tutorial-container">
-              <span id="tutorial-text">To add an item to your list, click the circle below</span>
-            </div>
-            <div className="input-container">
-              <img id="plus-button" src="./plus-button.svg" alt="Circle button with plus sign in the middle" />
-              <input
-                id="input-field"
-                className="hidden"
-                type="text"
-                placeholder="Add something..."
-              />
-            </div>
-          </div>
-        </main>
-      );
-    }
+    <div className="flex container">
+      <div>
+        <ul id="to-dos"></ul>
+      </div>
+      <div className="tutorial-container">
+        <span id="tutorial-text">To add an item to your list, click the circle below</span>
+      </div>
+      <SwitchInput isTyping={typing}/>
+    </div>
+  )
+}
     
 export default App;
